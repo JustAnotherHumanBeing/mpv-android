7d6a990dbf91c2cd87c2b36b9247c4a63a5a455f
Oleg <oleglelchuk@gmail.com>
2026-09-01 16:28:55 -0400

    build: pin dual-PID Dolby Vision mpv

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -46,7 +46,7 @@ v_ci_arch=arm64
 v_ci_ffmpeg_repo=https://github.com/JustAnotherHumanBeing/FFmpeg.git
 v_ci_ffmpeg=a38ca389b1a6ca6a672591fda5b9f9d129b01d00
 v_ci_mpv_repo=https://github.com/JustAnotherHumanBeing/mpv.git
-v_ci_mpv=a0f7fbc84bcfd9f03a5d06da52494d5cc5a892b6
+v_ci_mpv=06bb8e684c86eb271790a7f2e27dc1b35e014a0b
 v_ci_libplacebo_repo=https://github.com/JustAnotherHumanBeing/libplacebo.git
 v_ci_libplacebo=b3ff1dbe73de8e75bda36836f7b1b5a2e00068f1
 
